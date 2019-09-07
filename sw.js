308044e5aa63a57d14b311ea9379140e977d24fe
Tate Peñaranda <tate@tpenaranda.com>
2019-09-06 22:46:35 -0300

    Added Service Worker and cache.

---
+++ sw.js
@@ -0,0 +1,28 @@
+const CACHE_NAME = 'v1';
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((resp) => {
+      return resp || fetch(event.request).then((response) => {
+        return caches.open(CACHE_NAME).then((cache) => {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+self.addEventListener('activate', function(event) {
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (cacheName != CACHE_NAME) {
+            caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
